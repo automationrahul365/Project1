fca9e493becdd67dca3ad42fbb2717fdb9024522
automationrahul365 <automationrahul365@gmail.com>
2022-12-08 17:28:59 +0530

    first commnit

---
+++ Project1/src/pkg1/student.java
@@ -0,0 +1,13 @@
+package pkg1;
+
+public class student 
+{
+	public void m1()
+	{
+		System.out.println("Student class method 1");
+	}
+	public void m2()
+	{
+		System.out.println("Student class method 2");
+	}
+}
